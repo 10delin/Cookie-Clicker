7cc0dc6c1d5917137f8e36e181d0a6c7404decd9
Antonio Delage <delincruzado_20@hotmail.com>
2023-03-06 12:54:27 +0100

    feat(Game): create new page component

---
+++ src/pages/Game.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import Header from "../components/Header";
+
+const Game = () => {
+  const [points, setPoints] = useState(0);
+  const [autoClickers, setAutoClickers] = useState(0);
+  const [showAuto, setShowAuto] = useState(false);
+
+  const autoClickerCost = 50 + 50 * autoClickers;
+
+  const handleClick = () => {
+    setPoints((prevPoints) => prevPoints + 1);
+  };
+
+  const handleBuyAutoClicker = () => {
+    if (points >= autoClickerCost) {
+      setAutoClickers((prevAutoClickers) => prevAutoClickers + 1);
+      setPoints((prevPoints) => prevPoints - autoClickerCost);
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setPoints((prevPoints) => prevPoints + autoClickers);
+    }, 100);
+
+    return () => clearInterval(intervalId);
+  }, [autoClickers]);
+
+  useEffect(() => {
+    if (points >= 0) {
+      setShowAuto(true);
+    }
+  }, [points]);
+
+  return (
+    <>
+      <Header />
+      <div className="game">
+        <h1>Hola Antonio</h1>
+        <button onClick={handleClick}>Points: {points}</button>
+        {showAuto && (
+          <button
+            disabled={points < autoClickerCost}
+            onClick={handleBuyAutoClicker}
+          >
+            Buy AutoClicker ({autoClickerCost})
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Game;
