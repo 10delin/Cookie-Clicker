fa75cf8d349fefd4c948f3fd5edfc005dbac23a1
Antonio Delage <delincruzado_20@hotmail.com>
2023-03-06 12:58:16 +0100

    feat(Home): create new page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const Home = () => {
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="home">
+      <h1>Create a new player</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          name={name}
+          type="text"
+          placeholder="Name *"
+          onChange={handleChange}
+        />
+        <button disabled={name.length <= 0}>Join</button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
