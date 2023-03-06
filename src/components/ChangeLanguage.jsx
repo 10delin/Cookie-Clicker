f23d792b15d10112b8e56f8a34097cb856d4353a
Antonio Delage <delincruzado_20@hotmail.com>
2023-03-06 15:25:47 +0100

    feat(i18n): add new changeLanguage component

---
+++ src/components/ChangeLanguage.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+import SpainIcon from "../assets/images/spain.webp";
+import UkIcon from "../assets/images/united-kingdom.webp";
+
+const ChangeLanguage = () => {
+  const { i18n } = useTranslation();
+  const [language, setLanguage] = useState(i18n.language);
+
+  useEffect(() => {
+    i18n.changeLanguage(language);
+  }, [language, i18n]);
+
+  const classLanguageSpanish =
+    language === "es"
+      ? "change-language__button--active"
+      : "change-language__button";
+
+  const classLanguageEngish =
+    language === "en"
+      ? "change-language__button--active"
+      : "change-language__button";
+
+  return (
+    <div className="change-language">
+      <div className={classLanguageSpanish} onClick={() => setLanguage("es")}>
+        <img src={SpainIcon} alt="Spanish" />
+      </div>
+      <div className={classLanguageEngish} onClick={() => setLanguage("en")}>
+        <img src={UkIcon} alt="English" />
+      </div>
+    </div>
+  );
+};
+
+export default ChangeLanguage;
