98f9f32ae63ae5a4104432909695e1579a2d2e67
Antonio Delage <delincruzado_20@hotmail.com>
2023-03-07 11:26:05 +0100

    feat(icons): add new component MouseIcon

---
+++ src/components/Icons/MouseIcon.jsx
@@ -0,0 +1,11 @@
+import { MdMouse } from "react-icons/md";
+
+const MouseIcon = () => {
+  return (
+    <div className="mouse-icon">
+      <MdMouse />
+    </div>
+  );
+};
+
+export default MouseIcon;
